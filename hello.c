d8f6d6a3944a63fa3546a716e4f636d92abad953
zerolionsee <hekun6688@163.com>
2022-02-12 15:48:05 +0800

    改配置文件

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-main()
+int main()
 {
-    printf("hello world\n");
-
-    // system("pause");
+    char ch='A';//'""  
+    int  age=20;
+    float weight  =2;
+    double  c=3;
+    printf("i love u\n %c\n%d\n%f\n%lf\n",ch,age,weight,c);
+    printf("%d\n",sizeof(ch));
+    printf("%d\n",sizeof(age));
+    printf("%d\n",sizeof(weight));
+    printf("%d\n",sizeof(c));
+    return 0;
+    
+    
 }
