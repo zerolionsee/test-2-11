a10d9786f7201f2b92b88ae72e2328f447043d46
zerolionsee <hekun6688@163.com>
2022-02-14 17:01:33 +0800

    练习

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    char ch='A';//'""  
+    int num1=0,num2=10;
+    scanf("%d%d",&num1,&num2);
+    int sum=0;
+    sum=num1+num2;
+    printf("sum=%d\n",sum);
+    return 0;
+    // 引入外部函数 external;
+    /* char ch='A';//'""
     int  age=20;
     float weight  =2;
     double  c=3;
@@ -10,7 +17,7 @@ int main()
     printf("%d\n",sizeof(age));
     printf("%d\n",sizeof(weight));
     printf("%d\n",sizeof(c));
-    return 0;
-    
-    
+    return 0; */
+
+
 }
