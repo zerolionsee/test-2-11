21b3b7474715c76f57773edc1382c866479719db
zerolionsee <hekun6688@163.com>
2022-03-15 14:05:32 +0800

    調試

--- hello.c
+++ hello.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    /* extern int g_val;
+    /* extern int g_val;/引入外部函数 extern
     printf("g_val=%d\n",g_val);
     return 0; */
-    int num1=0,num2=10;
+    int num1,num2;
     scanf("%d%d",&num1,&num2);
     int sum=0;
     sum=num1+num2;
     printf("sum=%d\n",sum);
     return 0;
-    // 引入外部函数 extern;
     /* char ch='A';'
     int  age=20;
     float weight  =2;
